1fec38a8fa17af50c0049948348aa0e1fe4140a5
はっしゅさん <149641384+hashsan@users.noreply.github.com>
2023-11-04 13:40:56 +0900

    Create fujiyama.js

---
+++ fujiyama.js
@@ -0,0 +1,152 @@
+/*
+v1 fujiyama reborn
+*/
+
+function iscode(line){
+  const re=/^-{3,}$/
+  return re.test(line)
+}
+function iskaigyo(line){
+  return line === ''
+}
+function ismark(line){
+  const re=/(「.+?(?:？|！)」)/  
+  //const re=/？」|！」/
+  return re.test(line)
+}
+function ishead(line){
+  const re=/^＃.+$/
+  return re.test(line)
+}
+function isimg(line){
+  const re= /\.(jpeg|jpg|png|bmp|gif|webp|avif)$/i
+  return re.test(line)
+}
+/*
+function islink(line){
+  const re=/＠[-a-zA-Z0-9@:%_\+.~#?&//=]+$/
+  return re.test(line)
+}
+*/
+function islink(line){
+  var re=/^(＠.+)[（\(](.+)[)\）]/
+  return re.test(line)
+}
+function islinkRag(line){
+  const re=/(＠.*)([-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(?:\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?|#.+\w|#)/i
+  return re.test(line)
+  
+}
+function makeatlinkRag(line){
+  const re=/(＠.*)([-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(?:\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?|#.+\w|#)/i
+  
+  const m = line.match(re)
+  const name = m.at(1)
+  const url = m.at(2)
+  return {name,url}
+}
+
+
+function makeatlink(line){
+  const re=/^(＠.+)[（\(](.+)[)\）]/
+  const m = line.match(re)
+  const name = m.at(1)
+  const url = m.at(2)
+  return {name,url}
+  //const tag =`<a href="${url}">${name}</a>`
+  //return tag
+}
+function makemark(line){
+  const re=/(「.+?(?:？|！)」)/
+  const ret = line.replace(re,"<mark>$1</mark>")
+  return ret
+  //return `<p>${line.replace(re,"<mark>$1</mark>")}</p>`
+}
+
+
+function escape(line){
+  if(typeof line !== 'string') {
+    return line;
+  }
+
+  return line.replace(/[&'`"<>]/g, function(match) {
+    return {
+      '&': '&amp;',
+      "'": '&#x27;',
+      '`': '&#x60;',
+      '"': '&quot;',
+      '<': '&lt;',
+      '>': '&gt;',
+    }[match]
+  });
+}
+
+
+function fujiyama(temp){
+  const br ='\n'  
+  const ary = _lip(temp)
+  //console.log(ary)
+  const tagary = _walk(ary)
+  //console.log(tagary)
+  const html = tagary.join(br)
+  //console.log(html)
+  return html
+}
+
+
+function _lip(temp){
+  const br = /\r\n|\n|\r/g;
+  const ary=temp.trimStart().split(br)  
+  return ary;
+}
+
+function _walk(ary){
+  const cls ='code'
+  var coding = false
+  const iscoding =()=>coding // living
+  const res=ary.map(line=>{
+
+    if(iscode(line)){
+      coding =!coding
+      return `<p>---</p>`
+    }
+
+    if(iscoding()){
+      return `<p class="${cls}">${escape(line)}</p>`
+    }
+
+    if(iskaigyo(line)){
+      return `<p><!--kaigyo--></p>`
+    }
+
+    if(ishead(line)){
+      return `<h1>${line}</h1>`
+    }
+
+    if(isimg(line)){
+      return `<img src="${line}">`
+    }
+
+    if(islink(line)){
+      const {name,url} = makeatlink(line)
+      return `<a href="${url}">${name}</a>`      
+    }
+    
+    if(islinkRag(line)){
+      const {name,url} = makeatlinkRag(line)
+      return `<a href="${url}">${name}</a>`      
+    }
+    
+
+    if(ismark(line)){
+      const marked = makemark(line)
+      return `<p>${marked}</p>`
+    }
+
+    return `<p>${line}</p>`
+  });
+
+  return res
+}
+
+if(window) window.fujiyama = fujiyama
